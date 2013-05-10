7b6b5c458652ab0906eba0b84afdbc2597ab45d1
Dennis Wilkins <dennis@deliciousmorsel.com>
2013-05-10 11:20:01 -0600

    Support for ZeroStates

--- Funnel.js
+++ Funnel.js
@@ -1,223 +1,275 @@
-var Funnel = Backbone.View.extend({
-  className: 'funnel',
+define([] , function () {
+  var Funnel = Backbone.View.extend({
+    className: 'funnel',
 
-  options: {
-    horizontalOrientation: true,
-    valueAttribute: 'value',
-    uniqueAttribute: 'id',
-    scale: 'linear', // d3 scales
-    gapBetweenWidth: 20, // px
-    width: 'auto',
-    height: 'auto',
-    axisSize: 40,
-    sortData: false,
-    colors: [
-      '#73D216',
-      '#204A87',
-    ]
-  },
+    options: {
+      horizontalOrientation: true,
+      valueAttribute: 'value',
+      uniqueAttribute: 'id',
+      scale: 'linear', // d3 scales
+      gapBetweenSize: 20, // px
+      width: 'auto',
+      height: 'auto',
+      axisSize: 40,
+      sortData: false,
+      color: '#EC644F',
 
-  events: {
-    'click .funnel-piece' : 'onFunnelPieceClick',
-    'click .x-axis' : 'onAxisClick'
-  },
-  
-  initialize: function () {
-    this.setup();
-    this.build();
-    _.defer(function (that) {
-      that.render();
-    } , this);
-  },
+      zeroStateMessage: '~ NO DATA ~',
+      zeroStateTemplate: false // underscore template
+    },
 
-  setup: function () {
-    this.getValueFromDataItem = _.bind(this.getValueFromDataItem , this);
-  },
+    events: {
+      'click .funnel-piece' : 'onFunnelPieceClick',
+      'click .x-axis' : 'onAxisClick'
+    },
+    
+    initialize: function () {
+      this.setup();
+      this.build();
+      _.defer(function (that) {
+        that.render();
+      } , this);
+    },
 
-  createScales: function () {
-    this.max = _.max(this.options.data , this.getValueFromDataItem);
-    this.categoryAxisScale = d3.scale[this.options.scale]()
-        .rangeRound([0, this.getGraphValueAxisSize()])
-        .domain([0, this.getValueFromDataItem(this.max)]);
-  },
+    setup: function () {
+      this.getValueFromDataItem = _.bind(this.getValueFromDataItem , this);
+    },
 
-  build: function () {
-    this.visualization = d3.select(this.el);
-  },
+    getScaleForData: function (data) {
+      var max = _.max(data , this.getValueFromDataItem);
+      return categoryAxisScale = d3.scale[this.options.scale]()
+          .rangeRound([0, this.getGraphValueAxisSize()])
+          .domain([0, this.getValueFromDataItem(max)]);
+    },
 
-  update: function (data) {
-    if (data) {
-      this.options.data = data;
-    }
-    this.render();
-  },
+    build: function () {
+      this.visualization = d3.select(this.el);
+    },
 
-  render: function () {
-    this.$el.height(this.getHeight());
-    if (this.options.sortData) {
+    update: function (data) {
+      if (data) {
+        this.options.data = data;
+      }
+      this.render();
+    },
+
+    render: function () {
+      this.$el.height(this.getHeight());
+
+      if (this.options.sortData) {
+        this.sortData();
+      }
+
+      this.renderClassNames(); 
+      if (!this.options.data.length) {
+        this.setupZeroState();
+      } else {
+        this.teardownZeroState();
+      }
+      var categoryAxisScale = this.getScaleForData(this.options.data);
+      this.renderAxis(categoryAxisScale);
+      this.renderFunnel(categoryAxisScale);
+    },
+
+    sortData: function () {
       this.options.data = _.sortBy(this.options.data , function (d) {
         return -this.getValueFromDataItem(d);
       } , this);
-    }
-    if (this.isHorizontal()) {
-      this.$el.addClass('horizontal-funnel').removeClass('vertical-funnel');
-    } else {
-      this.$el.removeClass('horizontal-funnel').addClass('vertical-funnel');
-    }
-    this.createScales();
-    this.renderAxis();
-    this.renderFunnel();
-  },
+    },
 
-  renderAxis: function () {
-    var that = this,
-      axes = this.visualization.selectAll('.funnel-axis')
-      .data(this.options.data , function (data) {
-        return data[that.options.uniqueAttribute];
-      });
-    // enter
-    axes.enter()
-      .append('div')
-      .attr('class' , 'funnel-axis');
-    //update
-    axes
-      .style(this.isHorizontal() ? 'left' : 'top' , function (data , i) {
-        return ((that.getPieceSize() * i) + (i * that.options.gapBetweenWidth)) + 'px';
-      })
-      .style(this.isHorizontal() ? 'top' : 'left' , 'auto')
-      .style(this.isHorizontal() ? 'width' : 'height' , this.getPieceSize() + 'px')
-      .style(this.isHorizontal() ? 'height' : 'width', this.options.axisSize + 'px')
-      .html(function(data , i) {
-        if (that.options.axisTemplate) {
-          return that.options.axisTemplate(data);
-        } else {
-          return that.getLabelFromDataItem(data);
-        }
-      });
-    axes.exit().remove();
-  },
+    renderClassNames: function () {
+      if (this.isHorizontal()) {
+        this.$el.addClass('horizontal-funnel').removeClass('vertical-funnel');
+      } else {
+        this.$el.removeClass('horizontal-funnel').addClass('vertical-funnel');
+      }
+    },
 
-  renderFunnel: function () {
-    var that = this;
-
-    var pieces = this.visualization.selectAll('.funnel-piece')
-      .data(this.options.data , function (data) {
-        return data[that.options.uniqueAttribute];
-      });
-
-    // enter
-    pieces.enter()
-      .append('div')
-      .attr('class' , 'funnel-piece');
-
-
-    // update
-    // build the squares
-    pieces
-      .attr('style' , '')
-      .style('border-' + (this.isHorizontal() ? 'left' : 'top') + '-color' , d3.rgb(this.options.colors[0]).darker().toString())
-      .style(this.isHorizontal() ? 'width' : 'height' , this.getPieceSize() + 'px')
-      .style('background-color' , this.options.colors[0])
-      .style(this.isHorizontal() ? 'top' : 'left' , function (data , i) {
-        var previous = that.options.data[i-1];
-        if (previous) {
-          this._previous = previous;
-          return  (Math.round(that.getGraphValueAxisSize()/2) - Math.round(that.categoryAxisScale(that.getValueFromDataItem(this._previous))/2)) + 'px';
-        } else {
-          this._previous = false;
-          return '0px';
-        }
-      })
-      .style(this.isHorizontal() ? 'height' : 'width' , function (data) {
-        this._valueSize = Math.round(that.categoryAxisScale(that.getValueFromDataItem(data)));
-        return this._valueSize + 'px';
-      })
-      .style(this.isHorizontal() ? 'left' : 'top' , function (data , i) {
-          return ((that.getPieceSize() * i) + Math.round(that.options.gapBetweenWidth * (i-1 >= 0 ? i-1 :0 ))) + 'px';
-      })
-      .style('border-' + (this.isHorizontal() ? 'top' : 'left') + '-width' , function (data , i) {
-        if (this._previous) {
-          var previousSize = that.categoryAxisScale(that.getValueFromDataItem(this._previous)),
-            _borderSize = Math.round((previousSize-this._valueSize)/2),
-            total = ((_borderSize * 2) + this._valueSize);
-          this._borderSize = _borderSize;
-          if (total > previousSize) {
-            this._borderSize--;
+    renderAxis: function (categoryAxisScale) {
+      var that = this,
+        axes = this.visualization.selectAll('.funnel-axis')
+        .data(this.options.data , function (data) {
+          return data[that.options.uniqueAttribute];
+        }),
+        pieceSize = this.getPieceSize(this.options.data);
+      // enter
+      axes.enter()
+        .append('div')
+        .attr('class' , 'funnel-axis');
+      //update
+      axes
+        .style(this.isHorizontal() ? 'left' : 'top' , function (data , i) {
+          return ((pieceSize * i) + (i * that.options.gapBetweenSize)) + 'px';
+        })
+        .style(this.isHorizontal() ? 'top' : 'left' , 'auto')
+        .style(this.isHorizontal() ? 'width' : 'height' , pieceSize + 'px')
+        .style(this.isHorizontal() ? 'height' : 'width', this.options.axisSize + 'px')
+        .html(function(data , i) {
+          if (that.options.axisTemplate) {
+            return that.options.axisTemplate(data);
+          } else {
+            return that.getLabelFromDataItem(data);
           }
-          return _borderSize + 'px';
-        } else {
-          this._borderSize = 0;
-          return '0px';
-        }
-      })
-      .style('border-' + (this.isHorizontal() ? 'bottom' : 'right') + '-width' , function () {
-        return this._borderSize ? this._borderSize + 'px' : '0px';
-      })
-      .style('border-' + (this.isHorizontal() ? 'left' : 'top') + '-width' , function () {
-        if (this._previous) {
-          return that.options.gapBetweenWidth + 'px';
-        } else {
-          return '0px';
-        }
-      });
+        });
+      axes.exit().remove();
+    },
 
+    renderFunnel: function (categoryAxisScale) {
+      var that = this;
+
+      var pieces = this.visualization.selectAll('.funnel-piece')
+        .data(this.options.data , function (data) {
+          return data[that.options.uniqueAttribute];
+        }),
+        pieceSize = this.getPieceSize(this.options.data);
+
+      // enter
+      pieces.enter()
+        .append('div')
+        .attr('class' , 'funnel-piece');
+
+
+      // update
+      // build the squares
       pieces
-        .exit()
-        .remove();
-  },
+        .attr('style' , '')
+        .style('border-' + (this.isHorizontal() ? 'left' : 'top') + '-color' , d3.rgb(this.options.color).darker().toString())
+        .style(this.isHorizontal() ? 'width' : 'height' , pieceSize + 'px')
+        .style('background-color' , this.options.color)
+        .style(this.isHorizontal() ? 'top' : 'left' , function (data , i) {
+          var previous = that.options.data[i-1];
+          if (previous) {
+            this._previous = previous;
+            return  (Math.round(that.getGraphValueAxisSize()/2) - Math.round(categoryAxisScale(that.getValueFromDataItem(this._previous))/2)) + 'px';
+          } else {
+            this._previous = false;
+            return '0px';
+          }
+        })
+        .style(this.isHorizontal() ? 'height' : 'width' , function (data) {
+          this._valueSize = Math.round(categoryAxisScale(that.getValueFromDataItem(data)));
+          return this._valueSize + 'px';
+        })
+        .style(this.isHorizontal() ? 'left' : 'top' , function (data , i) {
+            return ((pieceSize * i) + Math.round(that.options.gapBetweenSize * (i-1 >= 0 ? i-1 :0 ))) + 'px';
+        })
+        .style('border-' + (this.isHorizontal() ? 'top' : 'left') + '-width' , function (data , i) {
+          if (this._previous) {
+            var previousSize = categoryAxisScale(that.getValueFromDataItem(this._previous)),
+              _borderSize = Math.round((previousSize-this._valueSize)/2),
+              total = ((_borderSize * 2) + this._valueSize);
+            this._borderSize = _borderSize;
+            if (total > previousSize) {
+              this._borderSize--;
+            }
+            return _borderSize + 'px';
+          } else {
+            this._borderSize = 0;
+            return '0px';
+          }
+        })
+        .style('border-' + (this.isHorizontal() ? 'bottom' : 'right') + '-width' , function () {
+          return this._borderSize ? this._borderSize + 'px' : '0px';
+        })
+        .style('border-' + (this.isHorizontal() ? 'left' : 'top') + '-width' , function () {
+          if (this._previous) {
+            return that.options.gapBetweenSize + 'px';
+          } else {
+            return '0px';
+          }
+        });
 
-  getPieceSize: function () {
-    var size = this.isHorizontal() ? this.getWidth() : this.getHeight(),
-      dataLength = this.options.data.length;
-    if (dataLength === 1) {
-      return size;
-    } else {
-      var gapsCount = dataLength - 1,
-        withoutGaps = size - (gapsCount * this.options.gapBetweenWidth);
-      return Math.round(withoutGaps / dataLength);
+        pieces
+          .exit()
+          .remove();
+    },
+
+    getPieceSize: function (data) {
+      var size = this.isHorizontal() ? this.getWidth() : this.getHeight(),
+        dataLength = data.length;
+      if (dataLength === 1) {
+        return size;
+      } else {
+        var gapsCount = dataLength - 1,
+          withoutGaps = size - (gapsCount * this.options.gapBetweenSize);
+        return Math.round(withoutGaps / dataLength);
+      }
+    },
+
+    getValueFromDataItem: function (data) {
+      return data[this.options.valueAttribute];
+    },
+
+    getLabelFromDataItem: function (data) {
+      return data[this.options.labelAttribute || this.options.valueAttribute];
+    },
+
+    getWidth: function () {
+      return this.options.width == 'auto' ? this.$el.width() : this.options.width;
+    },
+
+    getHeight: function () {
+      return this.options.height == 'auto' ? this.$el.height() : this.options.height;
+    },
+
+    getGraphValueAxisSize: function () {
+      if (this.isHorizontal()) {
+        return this.getHeight() - this.getAxisSize();
+      } else {
+        return this.getWidth() - this.getAxisSize();
+      }
+    },
+
+    getAxisSize: function () {
+      return this.options.axisSize;
+    },
+
+    isHorizontal: function () {
+      return !!this.options.horizontalOrientation;
+    },
+
+    setupZeroState: function () {
+      this.teardownZeroState();
+      this.zeroState = this.getZeroStateContent();
+      this.$el.append(this.zeroState);
+      var zHeight = this.zeroState.outerHeight(true),
+        zWidth = this.zeroState.outerWidth(true);
+      this.zeroState.css({
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginTop: -Math.floor((zHeight/2)),
+        marginLeft: -Math.floor((zWidth/2)),
+      });
+    },
+
+    getZeroStateContent: function () {
+      var $el = $('<div class="funnel-zero-state"></div>');
+      if (this.options.zeroStateTemplate) {
+        return $el.html(this.options.zeroStateTemplate(this.options.template));
+      } else {
+        return $el.text(this.options.zeroStateMessage);
+      }
+
+    },
+
+    teardownZeroState: function () {
+      if (this.zeroState) {
+        this.zeroState.remove();
+        delete this.zeroState;
+      }
+    },
+
+    onFunnelPieceClick: function (evt) {
+      var data = evt.target.__data__;
+      if (data) {
+        this.trigger('select' , data , evt.target);
+      }
+    },
+
+    onAxisClick: function (evt) {
+      this.onFunnelPieceClick(evt);
     }
-  },
-
-  getValueFromDataItem: function (data) {
-    return data[this.options.valueAttribute];
-  },
-
-  getLabelFromDataItem: function (data) {
-    return data[this.options.labelAttribute || this.options.valueAttribute];
-  },
-
-  getWidth: function () {
-    return this.options.width == 'auto' ? this.$el.width() : this.options.width;
-  },
-
-  getHeight: function () {
-    return this.options.height == 'auto' ? this.$el.height() : this.options.height;
-  },
-
-  getGraphValueAxisSize: function () {
-    if (this.isHorizontal()) {
-      return this.getHeight() - this.getAxisSize();
-    } else {
-      return this.getWidth() - this.getAxisSize();
-    }
-  },
-
-  getAxisSize: function () {
-    return this.options.axisSize;
-  },
-
-  isHorizontal: function () {
-    return !!this.options.horizontalOrientation;
-  },
-
-  onFunnelPieceClick: function (evt) {
-    var data = evt.target.__data__;
-    if (data) {
-      this.trigger('select' , data , evt.target);
-    }
-  },
-
-  onAxisClick: function (evt) {
-    this.onFunnelPieceClick(evt);
-  }
+  });
+  return Funnel;
 });
